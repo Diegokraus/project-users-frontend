d2f4068c05cd38cb69c8f6dfdeda125e3db70532
Diegokraus <diegokraus@gmail.com>
2024-02-08 23:21:51 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Container, H1, Image, ContainerItems, InputLabel, Input, Button} from './styles'
+
+
+const App = () => {
+  return (
+    <Container>
+      <Image />
+      <ContainerItems>
+        <H1>Olá</H1>
+
+        <InputLabel>Nome</InputLabel>
+        <Input placeholder='Nome' />
+
+        <InputLabel>Idade</InputLabel>
+        <Input placeholder='Idade' />
+
+        <Button>Cadastrar</Button>
+
+      </ContainerItems>
+    </Container>
+  )
+}
+
+export default App

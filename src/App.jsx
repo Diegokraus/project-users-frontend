0368dfe8c0047377fbe629530fc47190b3dad478
Diegokraus <diegokraus@gmail.com>
2024-02-14 22:46:38 -0300

    Added feat: useState useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,53 @@
-import React from 'react'
-import {Container, H1, Image, ContainerItems, InputLabel, Input, Button} from './styles'
+import React, {useState, useRef} from 'react'
+import {Container, H1, Image, ContainerItems, InputLabel, Input, Button, User} from './styles'
+import People from './assets/people.svg'
+import Arrow from './assets/arrow.svg'
+import Trash from './assets/trash.svg'
 
 
 const App = () => {
+  const [users, setUsers] = useState([])
+  const inputName = useRef()
+  const inputAge = useRef()
+
+
+  function addNewUser(){
+    setUsers([...users, {id:Math.random(), name:inputName.current.value, age:inputAge.current.value}])
+
+
+  }
+
+  function deleteUser(userId){
+    const newUsers = users.filter(user => user.id !== userId)
+    setUsers(newUsers)
+  }
+
+
+
   return (
     <Container>
-      <Image />
+      <Image alt="logo-imagem" src={People} />
       <ContainerItems>
         <H1>Olá</H1>
 
         <InputLabel>Nome</InputLabel>
-        <Input placeholder='Nome' />
+        <Input ref={inputName} placeholder='Nome' />
 
         <InputLabel>Idade</InputLabel>
-        <Input placeholder='Idade' />
+        <Input ref={inputAge} placeholder='Idade' />
 
-        <Button>Cadastrar</Button>
+        <Button onClick={addNewUser}>Cadastrar <img alt="seta" src={Arrow} /></Button>
+
+        <ul>
+          {users.map(user => (
+            <User key={user.id}>
+              <p>{user.name}</p>
+              <p>{user.age}</p>
+
+              <button onClick={() => deleteUser(user.id)}><img src={Trash} alt="lata-lixo" /></button>
+            </User>
+          ))}
+        </ul>
 
       </ContainerItems>
     </Container>
--- src/styles.jsx
+++ src/styles.jsx
@@ -1,36 +1,113 @@
-import styled from 'styled-components'
-import Background from './assets/background1.svg'
+import styled from "styled-components";
+import Background from "./assets/background1.svg";
 
-export const Container = styled.div `
-    background: url("${Background}");
-    background-size: cover;
+export const Container = styled.div`
+  background: url("${Background}");
+  background-size: cover;
 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+`;
 
-`
+export const Image = styled.img`
+  margin-top: 30px;
+`;
 
-export const Image = styled.img `
+export const ContainerItems = styled.div`
+  background: linear-gradient(
+    157.44deg,
+    rgba(255, 255, 255, 0.6),
+    rgba(255, 255, 255, 0.6),
+    rgba(255, 255, 255, 0.15)
+  );
+  border-radius: 61px 61px 0 0;
+  padding: 50px 36px;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
 
+export const H1 = styled.h1`
+  font-weight: bold;
+  font-size: 34px;
+  line-height: 40px;
+  text-align: center;
+  color: #fff;
+  margin-bottom: 80px;
+`;
 
-`
+export const InputLabel = styled.p`
+  font-weight: bold;
+  font-size: 18px;
+  color: #eee;
+  margin-left: 25px;
+`;
 
-export const ContainerItems = styled.div `
+export const Input = styled.input`
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 14px;
+  width: 342px;
+  height: 58px;
+  border: none;
+  outline: none;
+  padding-left: 25px;
+  font-size: 20px;
+  color: #fff;
+  margin-bottom: 25px;
+`;
 
-`
+export const Button = styled.button`
+  width: 342px;
+  height: 74px;
+  background-color: rgba(0, 0, 0, 0.8);
+  border-radius: 14px;
+  border: none;
+  font-weight: bold;
+  font-size: 17px;
+  color: #fff;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
 
+  &:hover {
+    opacity: 0.8;
+  }
 
-export const H1 = styled.h1 `
+  &:active {
+    opacity: 0.5;
+  }
+`;
 
-`
+export const User = styled.li`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-top:20px;
 
-export const InputLabel = styled.p `
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 14px;
 
-`
+  width: 342px;
+  height: 58px;
 
-export const Input = styled.input `
+  border: none;
+  outline: none;
 
-`
+  p {
+    font-size: 20px;
+    line-height: 28px;
+    color: #fff;
+  }
 
-export const Button = styled.button `
-
-`
-
+  button {
+    background: none;
+    border: none;
+    cursor: pointer;
+  }
+`;
